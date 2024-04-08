c1d7213e5b91b2a66a591c8ff047ff8a52f95b02
enzogit <enzo_91_10@hotmail.com>
2024-04-08 19:07:00 -0300

    prueba de font y de styles

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
 
 export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'retosta': require('./assets/fonts/retosta.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
   return (
     <View style={styles.container}>
-      <Text>Changas App</Text>
+      <Text style= {styles.title}>Changas App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000635',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 20,
+
   },
+  title: {
+  fontFamily: 'retosta',
+  fontSize: 40,
+  color: '#ff9800',
+  }
 });
