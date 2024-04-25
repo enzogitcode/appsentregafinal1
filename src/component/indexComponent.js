40bdb4ed1f2105d5f1368ecdbe2d33686a00f485
enzogit <enzo_91_10@hotmail.com>
2024-04-25 02:34:53 -0300

    employeelist

--- src/component/indexComponent.js
+++ src/component/indexComponent.js
@@ -2,12 +2,12 @@ import Header from './Header'
 import JobsList from './JobsList'
 import JobSeeker from './JobSeeker'
 import SearchEmployee from './SearchEmployee'
-//import EmployeeList from './EmployeeList'
+import EmployeeList from './EmployeeList'
 
 export {
     Header,
     JobsList,
     JobSeeker,
     SearchEmployee,
-    //EmployeeList
+    EmployeeList
 }
