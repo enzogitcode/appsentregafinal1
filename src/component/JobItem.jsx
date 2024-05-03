4945217dae60869b37cc420eca0c4306bdfc02f6
enzogit <enzo_91_10@hotmail.com>
2024-05-03 01:22:20 -0300

    editando jobitem

--- src/component/JobItem.jsx
+++ src/component/JobItem.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 
 const JobItem = ({ jobTitle, jobAge, category, firm, location, otherdata, experience }) => {
   return (
-    <View>
+    <View style={styles.jobItemContainer}>
       <View style={styles.jobTitleContainer}><Text style={styles.jobTitleText}>{jobTitle}</Text></View>
-      <Text style={styles.textSyle}>{jobAge}</Text>
-      <Text style={styles.textSyle}>{category}</Text>
-      <Text style={styles.textSyle}>{firm}</Text>
-      <Text style={styles.textSyle}>{location}</Text>
-      <Text style={styles.textSyle}>{otherdata}</Text>
-      <Text style={styles.textSyle}>{experience}</Text>
+      <View style={styles.jobCardContainer}>
+        <Text style={styles.textSyle}>Categoría:</Text>
+        <Text >{category}</Text>
+        <Text style={styles.textSyle}>Empresa: {firm}</Text>
+        <Text style={styles.textSyle}>Edad buscada: {jobAge}</Text>
+        <Text style={styles.textSyle}>Ubicación: {location}</Text>
+        <Text style={styles.textSyle}>Requerimientos: {otherdata}</Text>
+        <Text style={styles.textSyle}>Años de Experiencia sugeridos:{experience}</Text>
+      </View>
     </View>
   )
 }
@@ -18,15 +21,28 @@ const JobItem = ({ jobTitle, jobAge, category, firm, location, otherdata, experi
 export default JobItem
 
 const styles = StyleSheet.create({
+  jobItemContainer: {
+    alignItems: 'right',
+    padding: 5,
+    borderwidth: 10,
+    bordercolor: 'black',
+  },
   jobTitleContainer: {
-    alignItems: 'center',
+    textAlign: 'center',
     padding: 10,
     backgroundColor: 'lightblue',
     borderTopStartRadius: 10,
     borderTopEndRadius: 10
   },
+  jobCardContainer: 
+  {
+    backgroundColor: 'lightgreen',
+    padding: 15
+  }
+  ,
   jobTitleText: {
     fontFamily: 'retosta',
-    fontSize: 25
+    fontSize: 25,
+    textAlign: 'center'
   }
 })
