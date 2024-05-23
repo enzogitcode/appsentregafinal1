39a52f538b875620ee7bd09bf3616150542f919f
enzogit <enzo_91_10@hotmail.com>
2024-05-22 21:21:15 -0300

    edito los slices falta traer las categorías bien

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import globalReducer from '../features/Global/globalSlice'
 import { changasApi } from '../services/changasServices'
-import { setupListeners } from '@reduxjs/toolkit/query' 
+import { setupListeners } from '@reduxjs/toolkit/query'
+import jobsSlice from './employeeSlice/jobsSlice'
 
 const store = configureStore ({
     reducer: {
     global: globalReducer,
+    jobs: jobsSlice,
     [changasApi.reducerPath]: changasApi.reducer
 
 
