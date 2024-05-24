26c3209c1b65b91fec8b8f89b3a29a99e5dcb681
enzogit <enzo_91_10@hotmail.com>
2024-05-24 19:20:11 -0300

    auth services

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,14 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { baseAuthUrl, apiKey } from '../database/users'
+export const authApi = createApi({
+  reducePath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: baseAuthUrl }),
+  endpoints: (builder) => {
+    signUp: builder.mutation({
+      query: ({ ...auth }) => ({
+        url: `/accounts:signUp?key=${apiKey}`,
+        method: "POST",
+        body: auth
+      })
+    })
+  }
+})
 
-const authServices = () => {
-  return (
-    <View>
-      <Text>authServices</Text>
-    </View>
-  )
-}
-
-export default authServices
-
-const styles = StyleSheet.create({})
