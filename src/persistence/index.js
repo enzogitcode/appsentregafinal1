561ec25fb78af2d9e6701e13c2de4bf07f1b4aa9
enzogit <enzo_91_10@hotmail.com>
2024-06-12 00:26:03 -0300

    empiezo expo SQLite

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -2,10 +2,15 @@ import * as ExpoSQLite from "expo-sqlite"
 
 const db = ExpoSQLite.openDatabase("sessions.db")
 
-export const init = () => {
+export const initSQLiteDB = () => {
+    console.log("Will create table")
     const promise = new Promise((resolve, reject) => {
         db.transaction(tx => {
-            tx.executeSql('')
+            tx.executeSql("CREATE TABLE IF NOTE EXISTS sessions (localId TEXT PRIMARY KEY NOT NULL, email TEXT NOT NULL, token TEXT NOT NULL);"),
+            [], 
+                (_, result) => resolve(result), 
+                (_, error) => reject(error)
         })
     })
+    return promise
 }
